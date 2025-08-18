4bfed1ed19bbcd55c9b8a75cd0d0e3358dedd237
Chris Kateera <chris.kateera@mohara.co>
2025-08-18 10:00:00 +0000

    Add manifest.ts: initial manifest with datastores and scopes

---
+++ manifest.ts
@@ -0,0 +1,27 @@
+import { Manifest } from "deno-slack-sdk/mod.ts";
+import { Question } from "./datastores/question.ts";
+import { UserProfile } from "./datastores/user_profile.ts";
+import { UserSeenQuestion } from "./datastores/user_seen_question.ts";
+import { LeaderboardDaily } from "./datastores/leaderboard_daily.ts";
+
+export default Manifest({
+  name: "aws-quizzer",
+  description: "Daily AWS quizzes + leaderboards",
+  icon: "assets/icon.png",
+  workflows: [], // add workflows after scaffolding
+  datastores: [Question, UserProfile, UserSeenQuestion, LeaderboardDaily],
+  // Required scopes for features used in this scaffold
+  botScopes: [
+    "chat:write",
+    "commands",
+    "app_mentions:read",
+    "users:read",
+    "triggers:write",
+    "datastore:read",
+    "datastore:write",
+    "reactions:write",
+    "views:write",
+  ],
+});
+
+
